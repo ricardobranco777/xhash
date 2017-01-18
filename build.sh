e558fe9740b46c15e84567b032f62e7842959c11
Ricardo Branco <ricardo.branco@smartmatic.com>
2017-01-18 17:45:38 -0500

    Add build script

---
+++ build.sh
@@ -0,0 +1,22 @@
+#!/bin/bash -e
+
+gcc -o sslv sslv.c -ldl
+
+libdir=$(dirname $(./sslv | sort -k3 -n | awk 'END { print $1 }'))
+
+if [[ $libdir =~ /usr/local/ssl ]] ; then	
+	includedir="/usr/local/ssl/include"
+else
+	includedir="/usr/include"
+fi
+
+get_files()
+{
+	GOROOT=${GOROOT:-${HOME}/go}
+	echo xhash.go $(find ${GOROOT}/src/github.com/ricardobranco777/dgst -name \*.go ! -name \*_test.go)
+}
+
+sed -ri "s%(^#cgo CFLAGS:) -I/usr/include%\1 -I${includedir}%" $(get_files)
+sed -ri "s%(^#cgo LDFLAGS: -lcrypto) -L/usr/lib%\1 -L${libdir}%" $(get_files)
+
+go build xhash.go
