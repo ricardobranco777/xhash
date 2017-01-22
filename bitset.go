a227b333f5f639c2bc15cdaeed43bda418b51ba2
Ricardo Branco <ricardo.branco@smartmatic.com>
2017-01-22 11:42:06 -0500

    bitset.go: Bug & readability fixes

--- bitset.go
+++ bitset.go
@@ -11,45 +11,61 @@ const (
 )
 
 type Bitset struct {
-	word  []word
-	count int
+	words []word
+	count int // Number of elements inserted
+	max   int // Keep track of the maximum number ever inserted
 }
 
-func NewBitset() *Bitset {
+// Returns a new Bitset. Set argument to the expected maximum number or -1.
+func NewBitset(max int) *Bitset {
+	words := 1
+	if max >= 0 {
+		words = max / bitsPerWord
+		if max%bitsPerWord != 0 {
+			words++
+		}
+	}
 	bs := new(Bitset)
-	bs.word = make([]word, 1)
+	bs.words = make([]word, words)
+	bs.max = max
 	return bs
 }
 
 func (bs *Bitset) Add(i int) {
-	if i >= len(bs.word)*bitsPerWord {
-		var newSet = make([]word, len(bs.word)+i/bitsPerWord+1)
-		copy(newSet, bs.word)
-		bs.word = newSet
+	if i >= len(bs.words)*bitsPerWord {
+		var newSet = make([]word, len(bs.words)+i/bitsPerWord+1)
+		copy(newSet, bs.words)
+		bs.words = newSet
 	}
-	bs.word[i/bitsPerWord] |= 1 << uint(i%bitsPerWord)
+	bs.words[i/bitsPerWord] |= 1 << uint(i%bitsPerWord)
 	bs.count++
+	if i > bs.max {
+		bs.max = i
+	}
 }
 
 func (bs *Bitset) Del(i int) {
-	bs.word[i/bitsPerWord] &= ^(1 << uint(i%bitsPerWord))
+	bs.words[i/bitsPerWord] &= ^(1 << uint(i%bitsPerWord))
 	bs.count--
 }
 
 func (bs *Bitset) Test(i int) bool {
-	return (bs.word[i/bitsPerWord] & (1 << uint(i%bitsPerWord))) != 0
+	return (bs.words[i/bitsPerWord] & (1 << uint(i%bitsPerWord))) != 0
 }
 
 func (bs *Bitset) SetAll() {
-	for i := range bs.word {
-		bs.word[i] = ^word(0)
+	for i := range bs.words {
+		bs.words[i] = ^word(0)
 	}
-	bs.count = len(bs.word) * bitsPerWord
+	bs.count = len(bs.words) * bitsPerWord
+	if bs.max >= 0 {
+		bs.count = bs.max + 1
+	}
 }
 
 func (bs *Bitset) ClearAll() {
-	for i := range bs.word {
-		bs.word[i] = 0
+	for i := range bs.words {
+		bs.words[i] = 0
 	}
 	bs.count = 0
 }
@@ -58,14 +74,19 @@ func (bs *Bitset) GetCount() int {
 	return bs.count
 }
 
+// Returns a slice of all numbers in the set
 func (bs *Bitset) GetAll() (s []int) {
-	for i, w := range bs.word {
+	for i, w := range bs.words {
 		if w == 0 {
 			continue
 		}
 		for bit := 0; bit < bitsPerWord; bit++ {
 			if w&(1<<uint(bit)) != 0 {
-				s = append(s, i*bitsPerWord+bit)
+				num := i*bitsPerWord + bit
+				if num > bs.max {
+					return
+				}
+				s = append(s, num)
 			}
 		}
 	}
