a0323a186982ffbab4955a06b925aec48d7749ac
Ricardo Branco <ricardo.branco@smartmatic.com>
2017-02-09 12:32:58 -0500

    bitset.go: Use ffs() to optimize bit search

--- bitset.go
+++ bitset.go
@@ -81,18 +81,45 @@ func (bs *Bitset) GetAll() (s []int) {
 	}
 	s = make([]int, 0, bs.count)
 	for i, w := range bs.words {
-		if w == 0 {
-			continue
-		}
-		for bit := 0; bit < bitsPerWord; bit++ {
-			if w&(1<<uint(bit)) != 0 {
-				num := i*bitsPerWord + bit
-				if num > bs.max {
-					return
-				}
-				s = append(s, num)
+		for {
+			if w == 0 {
+				break
 			}
+			bit := ffs(w)
+			num := i*bitsPerWord + int(bit)
+			if num > bs.max {
+				return
+			}
+			s = append(s, num)
+			w &= ^(1 << bit)
 		}
+	}
+	return
+}
+
+func ffs(w word) (bit uint) {
+	if w&0xffffffff == 0 {
+		bit += 32
+		w >>= 32
+	}
+	if w&0xffff == 0 {
+		bit += 16
+		w >>= 16
+	}
+	if w&0xff == 0 {
+		bit += 8
+		w >>= 8
+	}
+	if w&0xf == 0 {
+		bit += 4
+		w >>= 4
+	}
+	if w&0x3 == 0 {
+		bit += 2
+		w >>= 2
+	}
+	if w&0x1 == 0 {
+		bit++
 	}
 	return
 }
