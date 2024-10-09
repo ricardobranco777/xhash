15d12201244d7de8c06aeec811d9b24465f6265f
Ricardo Branco <rbranco@suse.de>
2024-10-09 19:52:29 +0200

    Major refactoring

--- hash.go
+++ hash.go
@@ -9,68 +9,135 @@ import (
 	"sync"
 )
 
-func hashF(g *errgroup.Group, ctx context.Context, f io.ReadCloser, checksums []*Checksum) []*Checksum {
+// Hash file with just one algorithm
+func hashSmallF1(f io.ReadCloser, checksums []*Checksum) []*Checksum {
 	if checksums == nil {
 		checksums = getChosen()
 	}
 	initHashes(checksums)
-	if len(checksums) == 1 {
-		n, err := io.Copy(checksums[0], f)
-		if err != nil {
-			log.Print(err)
-			return nil
-		}
-		checksums[0].written = n
-		checksums[0].sum = checksums[0].Sum(nil)
-		return checksums
+
+	data, err := io.ReadAll(f)
+	if err != nil {
+		log.Print(err)
+		return nil
+	}
+	h := checksums[0]
+	if n, err := h.Write(data); err != nil {
+		log.Print(err)
+		return nil
+	} else {
+		h.written = int64(n)
+		h.sum = h.Sum(nil)
 	}
 
-	// Use goroutines if more than one algorithm
+	return checksums
+}
+
+// Hash small file with goroutines
+func hashSmallF(g *errgroup.Group, ctx context.Context, f io.ReadCloser, checksums []*Checksum) []*Checksum {
+	if checksums == nil {
+		checksums = getChosen()
+	}
+	if len(checksums) == 1 {
+		return hashSmallF1(f, checksums)
+	}
+	initHashes(checksums)
+
+	data, err := io.ReadAll(f)
+	if err != nil {
+		log.Print(err)
+		return nil
+	}
 
 	var wg sync.WaitGroup
-	wg.Add(len(checksums))
-	channels := make([]chan []byte, len(checksums))
-
-	for i, h := range checksums {
-		channels[i] = make(chan []byte)
+	for _, h := range checksums {
+		wg.Add(1)
 		g.Go(func() error {
 			defer wg.Done()
-			for data := range channels[i] {
-				n, err := h.Write(data)
-				if err != nil {
-					log.Print(err)
-					return err
-				}
-				h.written += int64(n)
+			if n, err := h.Write(data); err != nil {
+				log.Print(err)
+				return err
+			} else {
+				h.written = int64(n)
+				h.sum = h.Sum(nil)
 			}
-			h.sum = h.Sum(nil)
+			return nil
+		})
+	}
+	wg.Wait()
+
+	return checksums
+}
+
+// Hash file with just one algorithm
+func hashF1(f io.ReadCloser, checksums []*Checksum) []*Checksum {
+	if checksums == nil {
+		checksums = getChosen()
+	}
+	initHashes(checksums)
+
+	if n, err := io.Copy(checksums[0], f); err != nil {
+		log.Print(err)
+		return nil
+	} else {
+		checksums[0].written = n
+		checksums[0].sum = checksums[0].Sum(nil)
+	}
+
+	return checksums
+}
+
+// Hash file with goroutines
+func hashF(g *errgroup.Group, ctx context.Context, f io.ReadCloser, checksums []*Checksum) []*Checksum {
+	if checksums == nil {
+		checksums = getChosen()
+	}
+	if len(checksums) == 1 {
+		return hashF1(f, checksums)
+	}
+	initHashes(checksums)
+
+	var wg sync.WaitGroup
+	var writers []io.Writer
+	var pipeWriters []*io.PipeWriter
+
+	for _, h := range checksums {
+		pr, pw := io.Pipe()
+		writers = append(writers, pw)
+		pipeWriters = append(pipeWriters, pw)
+		wg.Add(1)
+		g.Go(func() error {
+			defer wg.Done()
+			if n, err := io.Copy(h, pr); err != nil {
+				log.Print(err)
+				return err
+			} else {
+				h.written = n
+				h.sum = h.Sum(nil)
+			}
 			return nil
 		})
 	}
 
 	wg.Add(1)
-	go func() {
+	g.Go(func() error {
 		defer wg.Done()
-		b := make([]byte, 64*KB) // TODO: Check best buffer size
-		for {
-			n, err := f.Read(b)
-			if err != nil {
-				if err != io.EOF {
-					log.Print(err)
-				}
-				break
+		defer func() {
+			for _, pw := range pipeWriters {
+				pw.Close()
 			}
-			bCopy := make([]byte, n)
-			copy(bCopy, b[:n])
-			for i := range channels {
-				channels[i] <- bCopy
-			}
+		}()
+		// build the multiwriter for all the pipes
+		mw := io.MultiWriter(writers...)
+		// copy the data into the multiwriter
+		if _, err := io.Copy(mw, f); err != nil {
+			log.Print(err)
+			return err
 		}
-		for i := range channels {
-			close(channels[i])
-		}
-	}()
+		return nil
+	})
 	wg.Wait()
+
 	return checksums
 }
 
@@ -96,7 +163,12 @@ func hashFile(g *errgroup.Group, ctx context.Context, input *Checksums) *Checksu
 		return nil
 	}
 
-	checksums := hashF(g, ctx, f, input.checksums)
+	var checksums []*Checksum
+	if info.Size() < 1e6 {
+		checksums = hashSmallF(g, ctx, f, input.checksums)
+	} else {
+		checksums = hashF(g, ctx, f, input.checksums)
+	}
 
 	return &Checksums{
 		file:      file,
