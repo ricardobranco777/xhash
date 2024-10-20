cfb6a79689f3f26b6d353762ecf297b7a760a8be
Ricardo Branco <rbranco@suse.de>
2024-10-20 13:21:33 +0200

    Bump version

--- main.go
+++ main.go
@@ -24,7 +24,7 @@ import (
 
 import flag "github.com/spf13/pflag"
 
-const version string = "3.5.0"
+const version string = "3.5.1"
 
 func getOutput(results *Checksums) []*Output {
 	outputs := make([]*Output, 0, len(results.checksums)+1)
